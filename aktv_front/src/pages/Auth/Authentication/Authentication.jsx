590a2d1990c4761b6a3445cc3386da7943ed981b
Romain68 <romain.klein.68720@gmail.com>
2025-06-20 16:14:02 +0200

    fix : auth page loading

--- aktv_front/src/pages/Auth/Authentication/Authentication.jsx
+++ aktv_front/src/pages/Auth/Authentication/Authentication.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Authentication.css";
-import aktv_logo from "../../../assets/icons/aktv_logo.png"
+import aktv_logo from "../../../assets/icons/aktv_logo.png";
+import { useState } from 'react';
+
 
 const Authentication = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+
 
   return (
     <div className="auth-container">
-      <div className="auth-card">
-        <img src={aktv_logo} alt="AKTV Logo" className="auth-logo" />
+      <div className="auth-card" style = {{display: !loading ? 'block' : 'none'}}>
+        <img src={aktv_logo} alt="AKTV Logo" className="auth-logo" onLoad={() => setLoading(false)} />
         <h1 className="auth-title">Bienvenue sur <span className="highlight">AKTV</span></h1>
         <p className="auth-description">
           Connecte-toi ou crée un compte pour accéder à ton fil d'actualités.
